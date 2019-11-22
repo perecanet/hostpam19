372003e8a9e233f6c7bea3275c95ce3a5a4a5a71
PereCanet <pere6cp@gmail.com>
2019-11-22 10:22:36 +0100

    scratch

---
+++ hostpam19:scratch/install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+useradd local1
+useradd local2
+useradd local3
+echo "local1" | passwd --stdin local1
+echo "local2" | passwd --stdin local2
+echo "local3" | passwd --stdin local3
+cp /opt/docker/nslcd.conf
+cp /opt/docker/nsswitch.conf
+
+
