657e222f4d6af8e455112d2d71e8ca6fb4e6a96e
PereCanet <pere6cp@gmail.com>
2019-11-25 08:44:02 +0100

    authconfig

--- hostpam19:scratch/install.sh
+++ hostpam19:scratch/install.sh
@@ -7,4 +7,5 @@ echo "local2" | passwd --stdin local2
 echo "local3" | passwd --stdin local3
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
+bash /opt/docker/auth.sh
 
