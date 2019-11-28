2b54cb347898855d7974d1d0ccbaa39491aa7c63
PereCanet <pere6cp@gmail.com>
2019-11-28 11:57:54 +0100

    pujada final

--- hostpam19:aware/exe_pam_python.sh
+++ hostpam19:aware/exe_pam_python.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-tar xvzf pam-python-1.0.6.tar.gz
-dnf -y install sphinx python3-sphinx phyton2-sphinx gcc
+tar xvzf pam-python-1.0.7.tar.gz
+dnf -y install sphinx python3-sphinx ppython-sphinx-locale.noarc gcc
+yum install -y make python-sphinx
 dnf -y install pam-devel
 dnf -y install redhat-rpm-config
 dnf -y install python-devel
